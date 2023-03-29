7116b1ae2bcf70d893325f12c5e50951e6617773
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-03-29 23:32:23 +0300

    Исправил выбрасывание ошибки при несуществующем id

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ToDo } from './models/ToDo.model';
 import { InjectModel } from '@nestjs/sequelize';
 
@@ -11,7 +11,11 @@ export class TodosService {
     }
 
     async findById(id: string): Promise<ToDo> {
-        return this.todoModel.findByPk(id);
+        const todo = await this.todoModel.findByPk(id);
+        if (!todo) {
+            throw new NotFoundException('ToDo not found');
+        }
+        return todo;
     }
 
     async create(todo: ToDo): Promise<ToDo> {
@@ -19,18 +23,12 @@ export class TodosService {
     }
 
     async update(id: string, todo: ToDo): Promise<ToDo> {
-        const existingToDo = await this.todoModel.findByPk(id);
-        if (!existingToDo) {
-            throw new Error('ToDo not found');
-        }
+        const existingToDo = await this.findById(id);
         return existingToDo.update(todo);
     }
 
     async delete(id: string): Promise<void> {
-        const todo = await this.todoModel.findByPk(id);
-        if (!todo) {
-            throw new Error('ToDo not found');
-        }
+        const todo = await this.findById(id);
         await todo.destroy();
     }
 
