19c66bc25d2e5e3873b5cc55e8ca17bf65f2c019
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-05 11:34:29 +0300

    move app files to dir, create empty todo.dto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { TodosModule } from './todos/todos.module';
+import { TodosModule } from '../todos/todos.module';
 
 @Module({
   imports: [TodosModule],
