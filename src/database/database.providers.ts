f03550d83b74246e9cb92aeed438503e74e717c2
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-15 11:43:57 +0300

    add token model to db

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -2,13 +2,14 @@ import { Sequelize } from 'sequelize-typescript';
 import { databaseConfig } from './database.config';
 import { ToDo } from 'src/models/ToDo.model';
 import { User } from 'src/models/User.model';
+import { Token } from 'src/models/Token.model';
 
 
 export const databaseProviders = [{
     provide: Sequelize,
     useFactory: async () => {
         const sequelize = new Sequelize(databaseConfig);
-        sequelize.addModels([User, ToDo]);
+        sequelize.addModels([ToDo, User, Token]);
         await sequelize.authenticate();
         await sequelize.sync();
         return sequelize;
