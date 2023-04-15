b2c9951197a2f831df5349ba87d90a3af31bb2eb
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-15 11:02:02 +0300

    add User model

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,13 +1,14 @@
 import { Sequelize } from 'sequelize-typescript';
 import { databaseConfig } from './database.config';
 import { ToDo } from 'src/todos/models/ToDo.model';
+import { User } from 'src/users/models/User.model';
 
 
 export const databaseProviders = [{
     provide: Sequelize,
     useFactory: async () => {
         const sequelize = new Sequelize(databaseConfig);
-        sequelize.addModels([ToDo]);
+        sequelize.addModels([User, ToDo]);
         await sequelize.authenticate();
         await sequelize.sync();
         return sequelize;
