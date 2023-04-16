cf0990a0712419098d20eb8c1ed3b5bd46fc8129
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-16 20:13:31 +0300

    add models from directory

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,15 +1,16 @@
 import { Sequelize } from 'sequelize-typescript';
 import { databaseConfig } from './database.config';
-import { ToDo } from 'src/models/ToDo.model';
-import { User } from 'src/models/User.model';
-import { Token } from 'src/models/Token.model';
+//import { ToDo } from 'src/models/ToDo.model';
+//import { User } from 'src/models/User.model';
+//import { Token } from 'src/models/Token.model';
 
 
 export const databaseProviders = [{
     provide: Sequelize,
     useFactory: async () => {
         const sequelize = new Sequelize(databaseConfig);
-        sequelize.addModels([ToDo, User, Token]);
+        //sequelize.addModels([ToDo, User, Token]);
+        sequelize.addModels([__dirname + 'src/models/*.model.ts']);
         await sequelize.authenticate();
         await sequelize.sync();
         return sequelize;
