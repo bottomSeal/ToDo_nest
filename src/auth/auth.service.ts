cb3203509cdbbbb02d4fd48ac4ce2580f320b97f
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-16 20:13:47 +0300

    add constructors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Token } from 'src/models/Token.model';
+import { User } from 'src/models/User.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @Inject('USERS_REPOSITORY')
+        private usersRepository: typeof User,
+
+        @Inject('TOKENS_REPOSITORY')
+        private tokensRepository: typeof Token
+    ) {}
+}
