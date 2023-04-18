2c4ffb3275523a9e85f47d39ffec55e5e80b2a2d
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-18 17:59:14 +0300

    add providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { UsersProvider } from 'src/users/users.providers';
+import { TokensProvider } from 'src/tokens/token.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, ...UsersProvider, ...TokensProvider]
 })
 export class AuthModule {}
