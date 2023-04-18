6f591b55fffabedeb3d510f578493c3b52779608
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-18 17:58:56 +0300

    add minLength for password

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,11 @@
-import { IsString, IsEmail } from "class-validator";
+import { IsString, IsEmail, MinLength } from "class-validator";
 
 export class CreateUserDto {
     @IsString()
     readonly username: string;
 
     @IsString()
+    @MinLength(8)   
     readonly password: string;
 
     @IsEmail()
