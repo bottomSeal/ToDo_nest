9588491e7d9bd83561b7ba9f9ac96d8408c1cca7
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-18 17:58:23 +0300

    add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TodosModule } from '../todos/todos.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TodosModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
