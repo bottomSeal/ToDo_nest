7567effb11a22af04100cec64b561704f48a397f
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-26 19:50:16 +0300

    add new requests

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,25 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { User } from 'src/models/User.model';
+import { SignInDto } from 'src/users/dto/sign-in.dto';
+import { Token } from 'src/models/Token.model';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('signUp')
-    async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
+    async signUp(@Body() createUserDto: CreateUserDto): Promise<String> { 
         return this.authService.signUp(createUserDto);
     }
 
+    @Get('signIn')
+    async signIn(@Body() signInDto: SignInDto): Promise<String> {
+        return this.authService.signIn(signInDto);
+    }
+
+    @Get('tokens')
+    async getAllTokensForUser(@Body() userId: string): Promise<Token[]> {
+        return this.authService.getAllTokensForUser(userId);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserAlreadyExistsException } from 'src/exceptions/user-already-exists.exeption';
 import { Token } from 'src/models/Token.model';
 import { User } from 'src/models/User.model';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { SignInDto } from 'src/users/dto/sign-in.dto';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +16,49 @@ export class AuthService {
         private tokensRepository: typeof Token
     ) {}
 
-    async signUp(user: CreateUserDto): Promise<User> {
-        return this.usersRepository.create({...user});
+    async verifyToken(xAccessToken: string): Promise<boolean> {
+        const accessToken = xAccessToken.slice(0, 20);
+        const userId = xAccessToken.slice(20);
+        const token = await this.tokensRepository.findOne({ where: { value: accessToken, userId: userId} });
+        if (!token) {
+            console.log("hasdads");
+            throw new UnauthorizedException('Invalid token');
+        }
+        return true;
+    }
+
+    async getAllTokensForUser(userId: string): Promise<Token[]> {
+        return this.tokensRepository.findAll();
+    }
+
+    async signUp(createUserDto: CreateUserDto): Promise<String> {
+        const { email, username, password } = createUserDto;
+
+        const userWithSameEmail = await this.usersRepository.findOne({ where: { email } });
+        if (userWithSameEmail) {
+            throw new UserAlreadyExistsException(`User with email ${email} already exists`);
+        }
+
+        const userWithSameUsername = await this.usersRepository.findOne({ where: { username } });
+        if (userWithSameUsername) {
+            throw new UserAlreadyExistsException(`User with username ${username} already exists`);
+        }
+
+        await this.usersRepository.create({...createUserDto});
+
+        return await this.signIn({ username, password });
+    }
+
+    async signIn(signInDto: SignInDto): Promise<String> {
+        const user = await this.usersRepository.findOne({ where: {...signInDto} }) 
+        if (!user) {
+            throw new UnauthorizedException('Invalid username or password');
+        }
+        
+        const tokenValue = nanoid(20);
+        await Token.create({ value: tokenValue, userId: user.userId });
+
+        const xAccessToken = tokenValue + user.userId;
+        return xAccessToken;
     }
 }
