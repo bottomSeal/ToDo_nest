e42a752107ba6fc4a957bbe9b89f9cb1881fa6a7
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-26 19:49:28 +0300

    add middleware

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 import { TodosProvider } from './todos.providers';
 import { DatabaseModule } from 'src/database/database.module';
+import { AuthMiddleware } from 'src/middlewares/auth.middleware';
+import { AuthService } from 'src/auth/auth.service';
+import { TokensProvider } from 'src/tokens/token.providers';
+import { UsersProvider } from 'src/users/users.providers';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [TodosController],
     providers: [TodosService, 
-        ...TodosProvider,
+        ...TodosProvider, 
+        AuthService,
+        ...TokensProvider,
+        ...UsersProvider
     ],
 })
-export class TodosModule {}
+export class TodosModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(AuthMiddleware) 
+            .forRoutes(TodosController); 
+    }
+}
