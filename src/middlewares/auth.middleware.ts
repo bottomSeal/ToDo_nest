e609a79058dbe98e8a4ad78ed7e11ba87fc8716f
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-26 19:49:58 +0300

    create auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { AuthService } from 'src/auth/auth.service';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+    constructor(private readonly authService: AuthService) {}
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const xAccessToken = req.headers['x-access-token'] as string;
+        if (!xAccessToken) {
+            return res.status(401).json({ message: 'Token not provided' });
+        }
+
+        try {
+            await this.authService.verifyToken(xAccessToken);
+            next();
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    }
+}
