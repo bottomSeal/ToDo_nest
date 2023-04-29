6d778bdbe37251631a532e09d691360ebed83e41
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-04-29 11:50:53 +0300

    add logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,24 +2,24 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { SignInDto } from 'src/users/dto/sign-in.dto';
-import { Token } from 'src/models/Token.model';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Post('signUp')
+    @Post('signup')
     async signUp(@Body() createUserDto: CreateUserDto): Promise<String> { 
         return this.authService.signUp(createUserDto);
     }
 
-    @Get('signIn')
+    @Get('signin')
     async signIn(@Body() signInDto: SignInDto): Promise<String> {
         return this.authService.signIn(signInDto);
     }
 
-    @Get('tokens')
-    async getAllTokensForUser(@Body() userId: string): Promise<Token[]> {
-        return this.authService.getAllTokensForUser(userId);
+    @Post('logout')
+    async logOut(req: Request): Promise<void> {
+        return this.authService.logOut( req['userId'] );
     }
 }
